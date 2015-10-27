bcc4d67d6f6d175a75af0dab11550d3492a108c3
Ian Andreu <ianandreu87@gmail.com>
2015-10-27 18:58:11 -0400

    complete overhaul - colorblock theme

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,18 @@
 console.log("we good..")
+
+
+// function scrollWin() {
+//     window.scrollBy(200,221);
+//     scrolldelay = setTimeout(pageScroll(),1000);
+// }
+
+// $('.scroll-top').click
+// document.getElementById("top-link").onclick = "pageScroll()"
+
+
+  $(document).ready(function (){
+    $('a[href=#top]').click(function(){
+        $('html, body').animate({scrollTop:0}, 3000);
+        $("html, body").animate({ scrollTop: $('html, body').prop("scrollHeight")}, 6000);
+    });
+});
