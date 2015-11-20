8a9360d80e38439681c479621e1e23f08c26a509
Ian Andreu <ianandreu87@gmail.com>
2015-11-19 20:37:26 -0500

    description divs slide in

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -12,27 +12,45 @@ $(document).ready(function (){
       // description div shows on mouse %, may want later
       // var newtop = e.clientY / 8;
       // $('.description').css({top: newtop+'%'});
-      $('#desc-0').show();
+      $('#desc-0').stop().animate({
+            right: "18px"
+        }, 300);
     }, function(){
-      $('#desc-0').hide();
+      $('#desc-0').stop().animate({
+            right: "-1500px"
+        }, 300);
     });
 
+
+
     $('.desc-1').hover(function(e){
-      $('#desc-1').show();
+      $('#desc-1').stop().animate({
+            right: "18px"
+        }, 300);
     }, function(){
-      $('#desc-1').hide();
+      $('#desc-1').stop().animate({
+            right: "-1500px"
+        }, 300);
     });
 
     $('.desc-2').hover(function(e){
-      $('#desc-2').show();
+      $('#desc-2').stop().animate({
+            right: "18px"
+        }, 300);
     }, function(){
-      $('#desc-2').hide();
+      $('#desc-2').stop().animate({
+            right: "-1500px"
+        }, 300);
     });
 
     $('.desc-3').hover(function(e){
-      $('#desc-3').show();
+      $('#desc-3').stop().animate({
+            right: "18px"
+        }, 300);
     }, function(){
-      $('#desc-3').hide();
+      $('#desc-3').stop().animate({
+            right: "-1500px"
+        }, 300);
     });
 
 
